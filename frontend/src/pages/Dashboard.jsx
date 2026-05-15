d879ec25e7db2e1dbfecade230d990d5e6c149ca
Abhishek Mane <abhishekmaneam20@gmail.com>
2026-05-15 15:31:43 +0530

    Added CorsConfig File

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,25 @@
+function Dashboard() {
+  return (
+    <div className="min-h-screen bg-black text-white p-10">
+
+      <h1 className="text-4xl font-bold mb-8">
+        DEVCOLLAB 🚀
+      </h1>
+
+      <div className="bg-zinc-900 p-6 rounded-2xl">
+
+        <h2 className="text-2xl font-bold">
+          Dashboard
+        </h2>
+
+        <p className="text-gray-400 mt-2">
+          Welcome to DEVCOLLAB
+        </p>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Dashboard
